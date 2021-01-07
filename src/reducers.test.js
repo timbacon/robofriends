a3ba63ecdd403ee939bea8785a6ba1d315c5538c
Tim Bacon <tim@thecrossplatform.net>
2021-01-07 21:12:08 +0200

    added tests for reducers

---
+++ src/reducers.test.js
@@ -0,0 +1,78 @@
+import { 
+    CHANGE_SEARCH_FIELD,
+    REQUEST_ROBOTS_FAILED,
+    REQUEST_ROBOTS_PENDING,
+    REQUEST_ROBOTS_SUCCESS 
+} from "./constants";
+
+import * as reducers from './reducers';
+
+describe('searchRobots', () => {
+    const initialStateSearch = {
+        searchField: ''
+    }
+    test('should return the initial state', () => {
+        expect(reducers.searchRobots(undefined, {})).toEqual({ searchField: ''})
+    })
+
+    test('should handle CHANGE_SEARCH_FIELD', () => {
+        expect(reducers.searchRobots(initialStateSearch, {
+            type: CHANGE_SEARCH_FIELD,
+            payload: 'abc'
+        })).toEqual({ searchField: 'abc' });
+    })
+    
+})
+
+describe('requestRobots', () => {
+    const initialStateRobots = {
+        robots: [],
+        isPending: false,
+        error: ''
+    };
+
+    test('should return initial state', () => {
+        expect(reducers.requestRobots(undefined, {})).toEqual(initialStateRobots);
+    })
+    
+    test('should handle REQUEST_ROBOTS_PENDING action', () => {
+        expect(reducers.requestRobots(initialStateRobots, {
+            type: REQUEST_ROBOTS_PENDING
+        })).toEqual({
+            robots: [],
+            isPending: true,
+            error: ''
+        });
+    })
+
+    test('should handle REQUEST_ROBOTS_SUCCESS action', () => {
+        expect(reducers.requestRobots(initialStateRobots, {
+            type: REQUEST_ROBOTS_SUCCESS,
+            payload: [{
+                id: '123',
+                name: 'test',
+                email: 'test@test.com'
+            }]
+        })).toEqual({
+            robots: [{
+                id: '123',
+                name: 'test',
+                email: 'test@test.com'
+            }],
+            isPending: false,
+            error: ''
+        });
+    })
+
+    test('should handle REQUEST_ROBOTS_FAILED action', () => {
+        expect(reducers.requestRobots(initialStateRobots, {
+            type: REQUEST_ROBOTS_FAILED,
+            payload: 'NOOOO!!!!'
+        })).toEqual({
+            robots: [],
+            isPending: false,
+            error: 'NOOOO!!!!'
+        });
+    })
+    
+})
